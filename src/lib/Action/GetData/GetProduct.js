bae3e30fae4734987e7dc511e328c79a0faeff82
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-08-12 15:53:44 +0600

    Get Data file Edit

--- src/lib/Action/GetData/GetProduct.js
+++ src/lib/Action/GetData/GetProduct.js
@@ -1,6 +1,6 @@
 export const GetProduct = async () => {
     try {
-        const res = await fetch("http://localhost:5000/api/product", { cache: "no-store" });
+        const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/Product`, { cache: "no-store" });
         if (!res.ok) return [];
         const data = await res.json();
         return Array.isArray(data) ? data : (data?.products || []);
--- src/lib/Action/PostData/PostProduct.js
+++ src/lib/Action/PostData/PostProduct.js
@@ -4,7 +4,7 @@
 
 export async function PostProduct(Data) {
     console.log(Data)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/Products`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/Product`, {
         method: 'POST',
         headers: {
 
