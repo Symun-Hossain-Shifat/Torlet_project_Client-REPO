39bb501e9c38974a51817be685a324bab79dd5e6
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-09-16 13:14:59 +0600

    Error solved

--- src/app/[locale]/providers.jsx
+++ src/app/[locale]/providers.jsx
@@ -1,16 +1,5 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
-
 export default function Providers({ children }) {
-    return (
-        <ThemeProvider
-            attribute="data-theme"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
-    );
-}
+    return <>{children}</>;
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html dir="ltr" suppressHydrationWarning>
+    <html dir="ltr" className="dark" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
